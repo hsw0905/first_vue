7e4a2180f8163cd8da1893931c9853a2ffb96866
hsw0905 <hsw0905@gmail.com>
2021-03-09 13:09:07 +0900

    같은 컴포넌트 레벨 간 통신 방법

---
+++ component-same-level/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const appHeader = {
+	template: '<div>header</div>',
+  props: ['propsdata'],
+}
+
+const appContent = {
+	template: '<div>content<button v-on:click="passNum">pass</button></div>',
+  methods: {
+	  passNum: function () {
+	    this.$emit('pass', 10);
+    }
+  }
+}
+new Vue({
+	el:'#app',
+	components: {
+		'app-header': appHeader,
+		'app-content': appContent,
+	},
+  data: {
+	  num: 0,
+  },
+  methods: {
+	  deliverNum : function (value) {
+      this.num = value;
+    }
+  }
+})
